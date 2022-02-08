68fef4c6ade52b8cd1fec5d170ce92c100183a47
Maxwell Barros <maxwellbarros@yahoo.com.br>
2022-02-08 20:28:34 -0300

    O State

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ import './assets/index.css';
 
 class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
-    this.notas = [];
+
+    this.state = {
+      notas: []
+    }
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    this.notas.push(novaNota);
-    console.log(this.notas.length);
+  criarNota(titulo, texto) {
+
+    if ((titulo !== "") && (texto !== "")) {
+      const novaNota = { titulo, texto };
+      const novoArrayNotas = [...this.state.notas, novaNota]
+      const novoEstado = 
+      {
+        notas: novoArrayNotas
+      }
+      this.setState(novoEstado)
+    }
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota = {this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
